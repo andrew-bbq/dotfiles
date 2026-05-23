778488d1734151148d74d4b9c7bc4138175b75b2
Andrew Bennett <awbennett@wisc.edu>
2026-05-23 12:25:15 -0700

    auto accept rust install

--- mac.sh
+++ mac.sh
@@ -20,4 +20,4 @@ brew install fzf
 brew install fd
 
 echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
